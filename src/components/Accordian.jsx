e61577d518db28b2a823304a87b2396a917db7cf
Sayan67 <sayan.das.21@aot.edu.in>
2024-01-12 23:13:44 +0530

    Completed the first project Accordian

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,30 +1,93 @@
 import React from 'react';
 import faqs from './data';
-import {useState} from 'react';
+import { useState } from 'react';
+
+const selectedArray = [
+    'none',
+    'none',
+    'none',
+    'none',
+];
 function Accordian() {
-    const [selected,useSelected] = useState(null);
-    function singleClickHandler(getId){
-     console.log(getId);   
+    const [selected, setSelected] = useState('none');
+    const [questionId, setQuestionId] = useState([]);
+    const [multiSelected, setMultiSelected] = useState(false);
+
+    //Checking if the answer is already opened and toggles the state.
+    function singleClickHandler(getId) {
+        if (selected === getId) {
+            setSelected('none');
+        } else {
+            setSelected(getId);
+        }
     }
-  return (
-    <>
-        <div>
-            {
-                faqs && faqs.length>0?
-                faqs.map((qs)=>{
-                    return(
-                        <div>
-                            <h3 onClick={()=>singleClickHandler(qs.id)}>{qs.question}</h3>
-                            <h3>+</h3>
-                        </div>
-                    )
-                })
-                : 
-                <div><h1>No data is present.</h1></div>
-            }
-        </div>
-    </>
-  )
+
+    //Multiple selected
+    function multiClickHandler(getId) {
+        let tempArr = [...questionId];
+        if (tempArr.indexOf(getId) !== -1) {
+            tempArr.splice(tempArr.indexOf(getId), 1);
+            console.log(tempArr);
+        } else {
+            tempArr.push(getId);
+            console.log(tempArr);
+        }
+        setQuestionId(tempArr);
+    }
+
+    return (
+        <>
+
+            <div className='Wrapper'>
+                <h1>Accordian</h1>
+                <button
+                    onClick={() => setMultiSelected(!multiSelected)}
+                >{multiSelected ? 'Enable Single select' : 'Enable Multi select'}</button>
+                {
+                    faqs && faqs.length > 0 ?
+                        faqs.map((qs, ind) => {
+                            return (
+                                <div className='main-div'>
+                                    <div className='sub-div'
+                                        onClick={
+                                            multiSelected ?
+                                                () => multiClickHandler(qs.id)
+                                                :
+                                                () => singleClickHandler(qs.id)
+                                        }
+                                    >
+                                        <h3 >{qs.question}</h3>
+                                        {
+                                            multiSelected ?
+                                                questionId.indexOf(qs.id) !== -1 ? <h3>-</h3> : <h3>+</h3>
+                                                :
+                                                selected === qs.id ? <h3>-</h3> : <h3>+</h3>
+                                        }
+
+                                    </div>
+                                    {
+                                        multiSelected ?
+                                            questionId.indexOf(qs.id) !== -1 ?
+                                                <div className='ans-div'>
+                                                    <h4>{qs.answer}</h4>
+                                                </div>
+                                                : null
+                                            :
+                                            selected === qs.id ?
+                                                <div className='ans-div'>
+                                                    <h4>{qs.answer}</h4>
+                                                </div>
+                                                : null}
+                                </div>
+
+                            )
+                        })
+                        :
+                        <div><h1>No data is present.</h1></div>
+                }
+            </div>
+        </>
+    )
 }
 
 export default Accordian
--- src/components/data.js
+++ src/components/data.js
@@ -1,23 +1,23 @@
 const faqs = [
     {
         id:'1',
-        question:"QS1",
-        answer:"ANS1",
+        question:"What is my name?",
+        answer:"My name is Sayan Das, aka Das.",
     },
     {
         id:'2',
-        question:"QS2",
-        answer:"ANS2",
+        question:"What am I pursuing currently?",
+        answer:"I am pursuing BTech in Computer Science and Engineering from Academy of Technology.",
     },
     {
         id:'3',
-        question:"QS3",
-        answer:"ANS3",
+        question:"What are my domain of interests?",
+        answer:"My main domain of interest is Web Development, more specifically frontend. Other than that I am interested in WEB3 and ML.",
     },
     {
         id:'4',
-        question:"QS4",
-        answer:"ANS4",
+        question:"What is my Tech Stack?",
+        answer:"I am currently using Reactjs(NextJS framework) along with Tailwid css, Styled components.",
     },
 ]
 export default faqs;
